ca19b8b6bebaab9d5ade269f57a52b4c5eff3b47
Kevin Tan <kvintan26@gmail.com>
2025-06-12 15:36:19 +0700

    Add Pickup Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -20,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone_number',
         'password',
     ];
 
@@ -44,5 +46,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
+    public function pickups() {
+        return $this->hasMany(Pickup::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool {
+        return $this->email == 'admin@gmail.com';
     }
 }
