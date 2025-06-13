c830bbd86a57b7bd4c5f1d8be9c85ce75d5717da
Kevin Tan <kvintan26@gmail.com>
2025-06-13 13:56:10 +0700

    Add Latest Orders Panel at Admin Page

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -22,6 +22,8 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
 
+    protected static ?string $recordTitleAttribute = 'name';
+
     public static function form(Form $form): Form
     {
         return $form
@@ -94,6 +96,11 @@ class UserResource extends Resource
         return [
             OrdersRelationManager::class
         ];
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['title', 'slug', 'author.name', 'category.name'];
     }
 
     public static function getPages(): array
