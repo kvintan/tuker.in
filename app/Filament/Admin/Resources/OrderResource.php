b2ad2e851be9dcc1ad0d829c1ab7716e2b7c3852
Kevin Tan <kvintan26@gmail.com>
2025-06-13 09:27:50 +0700

    Add Order Resource for Admin Page

--- app/Filament/Admin/Resources/OrderResource.php
+++ app/Filament/Admin/Resources/OrderResource.php
@@ -2,28 +2,127 @@
 
 namespace App\Filament\Admin\Resources;
 
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Order;
+use App\Models\Product;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Number;
+use Filament\Resources\Resource;
+use Filament\Actions\ActionGroup;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\Placeholder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Admin\Resources\OrderResource\Pages;
 use App\Filament\Admin\Resources\OrderResource\RelationManagers;
-use App\Models\Order;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Actions\DeleteAction;
 
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 //
+                Group::make()->schema([
+                    Section::make('Order Information')->schema([
+                        Select::make('user_id')
+                            ->label('Customer')
+                            ->relationship('user', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Select::make('payment_method')
+                            ->options([
+                                'stripe' => 'Stripe',
+                                'cod' => 'Cash on Delivery'
+                            ])
+                            ->required(),
+
+                        Select::make('currency')
+                            ->options([
+                                'idr' => 'IDR',
+                            ])
+                            ->default('idr')
+                            ->required(),
+
+                        Textarea::make('notes')
+                            ->columnSpanFull(),
+                    ])->columns(2),
+
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                            ->relationship()
+                            ->schema([
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->columnSpan(4)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', Product::find($state)?->price ?? 0))
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', Product::find($state)?->price ?? 0)),
+
+                                TextInput::make('quantity')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->columnSpan(2)
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount'))),
+
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+                            ])->columns(12),
+
+                            Placeholder::make('grand_total_placeholder')
+                                ->label('Grand Total')
+                                ->content(function (Get $get, Set $set) {
+                                    $total = 0;
+                                    if ($repeaters = $get('items')) {
+                                        return $total;
+                                    }
+
+                                    foreach ($repeaters as $key => $repeaters) {
+                                        $total += $get("items.{$key}.total_amount");
+                                    }
+                                    $set('grand_total', $total);
+                                    return Number::currency($total, 'IDR');
+                                }),
+
+                            Hidden::make('grand_total')
+                                ->default(0)
+                    ])
+                ])->columnSpanFull()
             ]);
     }
 
@@ -32,12 +131,43 @@ class OrderResource extends Resource
         return $table
             ->columns([
                 //
+                TextColumn::make('user.name')
+                    ->label('Customer')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('grand_total')
+                    ->numeric()
+                    ->sortable()
+                    ->money('IDR'),
+
+                TextColumn::make('payment_method')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('currency')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -53,11 +183,20 @@ class OrderResource extends Resource
         ];
     }
 
+    public static function getNavigationBadge(): ?string {
+        return static::getModel()::count();
+    }
+
+    public static function getNavigationBadgeColor(): string|array|null {
+        return static::getModel()::count() > 10 ? 'success' : 'danger';
+    }
+
     public static function getPages(): array
     {
         return [
             'index' => Pages\ListOrders::route('/'),
             'create' => Pages\CreateOrder::route('/create'),
+            'view' => Pages\ViewOrder::route('/{record}'),
             'edit' => Pages\EditOrder::route('/{record}/edit'),
         ];
     }
