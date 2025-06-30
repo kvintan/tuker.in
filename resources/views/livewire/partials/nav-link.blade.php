8386633040e0f47767f27e6eaba177212f47d3a7
Kevin Tan <kvintan26@gmail.com>
2025-06-30 21:11:35 +0700

    Fix the Href in the navbar

--- resources/views/livewire/partials/nav-link.blade.php
+++ resources/views/livewire/partials/nav-link.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false])
 
-<a {{ $attributes }}
+<a @if ($href) href="{{ $href }}" @endif {{ $attributes }} wire:navigate
     class="{{ $active ? 'underline text-black' : 'text-black' }} cursor-pointer rounded-md px-3 py-2 text-sm font-bold font-inter text-black hover:underline"
     aria-current="{{ $active ? 'page' : false }}">
     {{ $slot }}
