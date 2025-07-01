c1798932e5731716ec108b39f74987b779616260
Kevin Tan <kvintan26@gmail.com>
2025-07-01 21:21:07 +0700

    Add Auth to the route

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,35 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
+use App\Livewire\Auth\ForgotPassword;
+use App\Livewire\Auth\ResetPassword;
+use App\Livewire\PickupPage;
+use App\Livewire\Profile;
+use App\Livewire\Product;
+use App\Livewire\ProductDetail;
 use App\Livewire\Cart;
 use App\Livewire\Home;
 use App\Livewire\About;
 use App\Livewire\Auction;
-use App\Livewire\Product;
-use App\Livewire\Profile;
-use App\Livewire\Auth\Login;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Livewire\Auth\ForgotPassword;
-use App\Livewire\Auth\Register;
-use App\Livewire\Auth\ResetPassword;
-use App\Livewire\PickupPage;
-use App\Livewire\ProductDetail;
-
-Route::get('/product', [ProductController::class, 'index']);
 
 Route::get('/', Home::class);
 Route::get('/about', About::class);
-Route::get('/auction', Auction::class);
-Route::get('/cart', Cart::class);
-Route::get('/pickup', PickupPage::class);
-Route::get('/product', Product::class);
-Route::get('/product/{slug}', ProductDetail::class);
-Route::get('/profile', Profile::class);
-Route::get('/login', Login::class);
+
+// Auth pages (public)
+Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class);
 Route::get('/forgot-password', ForgotPassword::class);
 Route::get('/reset-password', ResetPassword::class);
+
+// Pages that require login
+Route::middleware(['auth'])->group(function () {
+  Route::get('/profile', Profile::class);
+  Route::get('/pickup', PickupPage::class);
+  Route::get('/cart', Cart::class);
+  Route::get('/product', Product::class);
+  Route::get('/product/{slug}', ProductDetail::class);
+  Route::get('/auction', Auction::class);
+});
