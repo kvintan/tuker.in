da0bb22595e27e85ed73f67dbb8cf75c9061b79d
Kevin Tan <kvintan26@gmail.com>
2025-07-02 21:27:12 +0700

    Favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <title>{{ $title ?? 'Tuker.in' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
